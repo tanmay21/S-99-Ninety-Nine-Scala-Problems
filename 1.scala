22eb4f41f4c85bd72a3196429363ab765ad8fb44
Tanmay Gupta <tg21phone@gmail.com>
2018-06-28 15:29:06 +0530

    added 1st file

---
+++ 1.scala
@@ -0,0 +1,12 @@
+import scala.io.StdIn
+import scala.math._
+import scala.collection.mutable.ArrayBuffer
+import java.io.PrintWriter
+import scala.io.Source
+
+object test_1{
+	def main(args:Array[String]){
+		val list=List(2,5,8,9,27)
+		print(list(list.length-1))
+	}
+}
