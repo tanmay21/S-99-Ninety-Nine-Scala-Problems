9db983abe7e9672c0329aac4587f554978cf0611
Tanmay Gupta <tg21phone@gmail.com>
2018-07-02 15:36:37 +0530

    adding more files

--- 8.scala
+++ 8.scala
@@ -1,4 +1,4 @@
-//Determine the greatest common divisor of two positive integer numbers.
+//Eliminate consecutive duplicates of list elements.
 import scala.io.StdIn
 import scala.math._
 import scala.collection.mutable.ArrayBuffer
@@ -7,7 +7,19 @@ import scala.io.Source
 
 object test_8{
 	def main(args:Array[String]){
-		var l = List(1,4,3,2,2,5,3,1)
-		print(l.distinct)
+		var list=scala.collection.mutable.ListBuffer(5,7,3,3,3,3,5,5,9,9,8,7,7,7,1,1,1)
+                                                  
+                                                
+		var i = 0                         
+		var l = list.length-1            
+		
+		while(i<l){
+			if(list(i)==list(i+1)){
+				list.remove(i+1)
+				i-=1
+				l-=1
+			}
+			i+=1
+		}
 	}
 }
